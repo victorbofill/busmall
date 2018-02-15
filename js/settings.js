a809788b76bb3e38b0295ed0882f671609f34ab3
Victor Bofill <terranvas@gmail.com>
2018-02-15 14:14:28 -0800

    Settings JS and html complete.

--- js/settings.js
+++ js/settings.js
@@ -1,9 +1,27 @@
 'use strict';
+
 const form = document.getElementById('settings-form');
+
+let Settings = {prodShown: 3, rounds: 25};
+
+if (localStorage.getItem('Settings')) {
+    Settings = JSON.parse(localStorage.getItem('Settings'));
+    form['prod-shown'].value = Settings.prodShown;
+    form.rounds.value = Settings.rounds;
+}
+
+localStorage.setItem('Settings', JSON.stringify(Settings));
 
 form.addEventListener('submit', function() {
     event.preventDefault();
 
-    const prodShown = this['prod-shown'].value;
-    const rounds = this.rounds.value;
+    const Settings = JSON.parse(localStorage.getItem('Settings'));
+
+    const newProdShown = this['prod-shown'].value;
+    const newRounds = this.rounds.value;
+
+    Settings.prodShown = newProdShown;
+    Settings.rounds = newRounds;
+
+    localStorage.setItem('Settings', (JSON.stringify(Settings)));
 });
